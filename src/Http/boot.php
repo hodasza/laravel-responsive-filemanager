412ea0a1b7fcda2576effff6b29431e8c85de3ca
Hodász Ágoston <hodasza@gmail.com>
2022-06-16 00:52:45 +0200

    Update boot.php

--- src/Http/boot.php
+++ src/Http/boot.php
@@ -4,7 +4,7 @@ use \ZanySoft\ResponsiveFileManager\RFM;
 
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
-mb_http_input('UTF-8');
+//mb_http_input('UTF-8');
 mb_language('uni');
 mb_regex_encoding('UTF-8');
 // ob_start('mb_output_handler');
